d29dd5c02e3e71b07a993dc2aff433b10259a3a9
juliusyoung <yangzuohui@gmail.com>
2017-01-14 23:55:09 +0800

    implement part 2

---
+++ app/controllers/admin/jobs_controller.rb
@@ -0,0 +1,11 @@
+class Admin::JobsController < ApplicationController
+  def index
+    @jobs = Job.all
+  end
+  def require_is_admin
+    if !current_user.admin?
+      flash[:alert] = 'You are not admin'
+      redirect_to root_path
+    end
+end
+end
